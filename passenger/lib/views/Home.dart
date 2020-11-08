304d9600a364f86eaabf6459f7efdba75572e3a0
Verns <smaverns@gmail.com>
2020-11-08 17:51:33 +0200

    Passenger home to Home

--- passenger/lib/views/Home.dart
+++ passenger/lib/views/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:passenger/widget/passengerSingleFeed.dart';
+import 'package:passenger/widget/toGoUpdate.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,10 +11,55 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Home'),
+      backgroundColor: Color(0xFFFCF9F9),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFFCF9F9),
+        elevation: 0,
+        title: Text(
+          'Inter-interests',
+          style: TextStyle(
+            color: Color(0xFF1B1B1B),
+            fontFamily: 'Roboto',
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(
+              Icons.search,
+              size: 25,
+              color: Color(0xFFCC1718),
+            ),
+          ),
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(
+              Icons.menu,
+              size: 25,
+              color: Color(0xFFCC1718),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              child: ListView(
+                shrinkWrap: false,
+                children: [
+                  ToGoUpdate(),
+                  SizedBox(height: 10.0),
+                  PassengerSingleFeed(),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
